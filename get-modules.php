5a066bc4d682a3b9d5ed200bb9576c9ab2e99149
tjespe <tordjohanespe@gmail.com>
2018-06-14 23:07:54 +0200

    Escaped special character

--- get-modules.php
+++ get-modules.php
@@ -4,9 +4,9 @@ function loadModules($libURL, ...$modules) {
   $css = "";
   $code = "window.dependencies = ".json_encode($modules).";\n";
   foreach ($modules as $module) {
-    $content = file_get_contents("$libURL/$module.js");
+    $content = file_get_contents("$libURL/src/$module.js");
     $code .= $content;
-    if (substr($content, 0, 6) === "//#CSS") $css .= file_get_contents("$libURL/../css/$module.css");
+    if (substr($content, 0, 6) === "//#CSS") $css .= str_replace("`", "\`", file_get_contents("$libURL/../css/$module.css"));
   }
   $code .= "document.querySelector('style').innerText += `$css`;";
   return $code;
