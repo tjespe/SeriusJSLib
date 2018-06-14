d35e0be6768f4712e27cf2eb9d4ac2eb51031e92
tjespe <tordjohanespe@gmail.com>
2018-06-14 23:16:05 +0200

    Hardcoded URL

--- get-modules.php
+++ get-modules.php
@@ -1,12 +1,13 @@
 <?php
 
-function loadModules($libURL, ...$modules) {
+function loadModules(...$modules) {
+  $libURL = "https://raw.githubusercontent.com/tjespe/UsefulModules/master";
   $css = "";
   $code = "window.dependencies = ".json_encode($modules).";\n";
   foreach ($modules as $module) {
     $content = file_get_contents("$libURL/src/$module.js");
     $code .= $content;
-    if (substr($content, 0, 6) === "//#CSS") $css .= str_replace("`", "\`", file_get_contents("$libURL/../css/$module.css"));
+    if (substr($content, 0, 6) === "//#CSS") $css .= str_replace("`", "\`", file_get_contents("$libURL/css/$module.css"));
   }
   $code .= "document.querySelector('style').innerText += `$css`;";
   return $code;
